ee7d619d7effd6ffed14cf1fc1fc47596633055b
Brady Bhalla <bradybhalla@Bradys-MacBook-Pro.local>
2022-01-30 21:33:29 -0700

    first commit

---
+++ neural-network.js
@@ -0,0 +1,25 @@
+function Neural_Network(arr){
+	this.layer_sizes = arr.slice();
+	this.weights = [];
+	this.biases = [];
+}
+
+
+
+
+function setup(){
+	const canvas = createCanvas(windowWidth*0.75,500);
+	canvas.parent("sketch");
+}
+
+function draw(){
+	background(200);
+}
+
+function mouseIn(){
+	return (mouseX > 0) && (mouseX < width) && (mouseY > 0) && (mouseY < height);
+}
+
+function windowResized(){
+	resizeCanvas(windowWidth*0.75, 500);
+}
