2e2ae68e34474a6defab360169515c15119b5433
brady <brady@brad.brad>
2022-03-22 22:55:06 -0600

    k-means clustering simulation

--- static/scripts/k-means.js
+++ static/scripts/k-means.js
@@ -3,3 +3,47 @@ data should be in the format:
 [{x:<x>, y:<y>}, {x:<x>, y:<y>}, ...]
 */
 
+function KMeans(data, num_clusters){
+	this.data = data;
+	this.n = num_clusters;
+	this.centroids = new Array(this.n).fill().map(()=>{
+		let data = this.data[floor(random()*this.data.length)];
+		return {x:data.x, y:data.y};
+	});
+}
+
+KMeans.prototype.label_data = function (){
+	for (const d of data){
+		const dist_to_centroids = this.centroids.map(c=>dist(d.x, d.y, c.x, c.y));
+		d.label = KMeans._argmin(dist_to_centroids);
+	}
+}
+
+// the data should be labeled first
+KMeans.prototype.update_centroids = function(){
+	this.centroids = this.centroids.map((c, i) => {
+		let filtered_data = this.data.filter(d=>d.label == i);
+		let total_pos = filtered_data.reduce((old, d) => {return {x:old.x+d.x, y:old.y+d.y}});
+		return {x:total_pos.x/filtered_data.length, y:total_pos.y/filtered_data.length};
+	});
+};
+
+KMeans._argmin = function(arr){
+	let min_index = 0;
+	let min_val = arr[0];
+	for (let i=1; i<arr.length; i++){
+		let new_val = arr[i];
+		if (min_val < new_val){
+			continue
+		} else if (min_val == new_val){
+			if (random() < 0.5){
+				continue
+			}
+		}
+
+		min_index = i;
+		min_val = new_val;
+	}
+
+	return min_index;
+};
--- static/scripts/supervised-unsupervised-sketch.js
+++ static/scripts/supervised-unsupervised-sketch.js
@@ -1,16 +1,95 @@
 let data;
-let solving = false;
+let data_name;
+let kmeans;
+let finding = false;
+let finding_start;
+
+let _seven_cluster_centers;
+
+const data_info = {
+	"Two Clusters": {
+		clusters: 2,
+		points: 100,
+		gen: ()=>{
+			return {x:width/20*randomGaussian() + width/3 + (random()<0.5?1:0)*width/3, y:width/20*randomGaussian() + height/2};
+		}
+	},
+	"Three Clusters": {
+		clusters: 3,
+		points: 150,
+		gen: ()=>{
+			const angle = 2*PI*floor(random()*3)/3;
+			return {x:width/20*randomGaussian() + width/2 + height/3*cos(angle), y:width/20*randomGaussian() + height/2 + height/3*sin(angle)};
+		}
+	},
+	"Seven Clusters": {
+		clusters: 7,
+		points: 500,
+		gen: ()=>{
+			const cluster = floor(random()*7);
+			return {x:width/40*randomGaussian() + _seven_cluster_centers[cluster].x, y:width/40*randomGaussian() + _seven_cluster_centers[cluster].y};
+		}
+	}
+}
+
+function find_clusters(){
+	kmeans = new KMeans(data, data_info[data_name].clusters);
+	kmeans.label_data();
+	finding = true;
+	finding_start = frameCount;
+}
+
+function get_hue(label){
+	return label/data_info[data_name].clusters*360;
+}
 
 function setup(){
 	const canvas = createCanvas(windowWidth*0.75,500);
 	canvas.parent("sketch");
 
-	
+	randomSeed(7);
+	_seven_cluster_centers = new Array(7).fill().map(()=>{return {x:random()*width/2+width/4, y:random()*height/2+height/4}});
 
+	$("#data-select").change(()=>{
+		data_name = $("#data-select option:selected").text();
+
+		finding = false;
+		data = new Array(data_info[data_name].points).fill().map(()=>data_info[data_name].gen());
+
+	});
+
+	$("#data-select").change();
+
+	rectMode(CENTER);
+	colorMode(HSB);
+	frameRate(30);
+	noStroke();
 }
 
 function draw(){
-	background(200);
+	background(0,0,78);
+	
+	if (finding){
+		if ((frameCount-finding_start)%30 == 29){
+			kmeans.update_centroids();
+			kmeans.label_data();
+		}
+
+		for (const d of data){
+			fill(get_hue(d.label), 50, 50);
+			ellipse(d.x, d.y, 10, 10);
+		}
+		
+		for (const i in kmeans.centroids){
+			fill(get_hue(i), 80, 90);
+			rect(kmeans.centroids[i].x, kmeans.centroids[i].y, 10, 10);
+		}
+	} else {
+		for (const d of data){
+			fill(0, 0, 0);
+			ellipse(d.x, d.y, 10, 10);
+		}
+	}
 }
 
 function mouseIn(){
@@ -18,7 +97,10 @@ function mouseIn(){
 }
 
 
-
 function windowResized(){
 	resizeCanvas(windowWidth*0.75, 500);
+}
+
+function wait_promise(ms){
+	return new Promise((res, rej)=>setTimeout(res, ms));
 }
